21e5b4b2f65551c1814448347a03a934f53535fb
Jordan Montalvo <davidmontalvo59@gmail.com>
2024-07-16 15:00:10 +0000

    feat: handle input and query the database

--- element.sh
+++ element.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 if [[ -z $1 ]]; then echo "Please provide an element as an argument." exit; fi
+if [[ $1 =~ ^[0-9]+$ ]]; then ELEMENT=$($PSQL "SELECT ... WHERE atomic_number = $1") else ELEMENT=$($PSQL "SELECT ... WHERE symbol = '$1' OR name = '$1'"); fi
