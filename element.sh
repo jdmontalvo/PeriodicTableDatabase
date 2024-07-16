280e3d948cd433fb6b5f82250501119da9832071
Jordan Montalvo <davidmontalvo59@gmail.com>
2024-07-16 15:00:10 +0000

    chore: set executable permissions for script

--- element.sh
+++ element.sh
@@ -1,1 +1,3 @@
-echo 'Please provide an element as an argument.'
+#!/bin/bash
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+if [[ -z $1 ]]; then echo "Please provide an element as an argument." exit; fi
